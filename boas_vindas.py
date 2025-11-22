c06d061f7aacef58b9ad5df3f2f86aed69941e3a
lisasalesss <115100317+lisasalesss@users.noreply.github.com>
2025-11-21 22:48:49 -0300

    Add welcome message function in boas_vindas.py

---
+++ boas_vindas.py
@@ -0,0 +1,15 @@
+from utils import limpar_tela, pausar
+
+
+def boas_vindas():
+    """Exibe a mensagem de boas-vindas e apresentação do projeto"""
+    limpar_tela()
+    print("●" * 50)
+    print("🎵  SEJA BEM-VINDO AO MAKE MUSIC GREEN!  🌱")
+    print("●" * 50)
+    print("\nO MakeMusicGreen une música e sustentabilidade!")
+    print("Quanto mais você ouve música, mais você aprende")
+    print("sobre práticas sustentáveis através do nosso quiz.")
+    print("\n🌍 Cada ação importa para um planeta melhor! 🌍")
+    print("●" * 50)
+    pausar()

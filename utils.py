2e30959afc9cdb3cf5a7ee7ee0dd3d76d1e41f71
lisasalesss <115100317+lisasalesss@users.noreply.github.com>
2025-11-21 22:52:38 -0300

    Add utility functions for clearing screen and pausing

---
+++ utils.py
@@ -0,0 +1,10 @@
+# utils.py - Funções Utilitárias
+
+def limpar_tela():
+    """Limpa a tela do console"""
+    print("\n" * 50)
+
+
+def pausar():
+    """Pausa o programa até o usuário pressionar ENTER"""
+    input("\nPressione ENTER para continuar...")
